11c35641b644692ca16f08ee8a350c6fc6a06f64
hyosukjung <hyosukjung32@gmail.com>
2025-04-21 14:34:48 +0900

    프로젝트 생성 및 초기 세팅 진행중(router)

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,7 @@
+import { createRouter, createWebHistory } from 'vue-router' import HomeView from '../views/HomeView.vue'
+
+const routes = [ { path: '/', name: 'home', component: HomeView, }, { path: '/auth', name: 'auth', component: () => import('../views/AuthView.vue'), }, ]
+
+const router = createRouter({ history: createWebHistory(), routes, })
+
+export default router
